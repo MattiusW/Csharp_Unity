9f61fbd385e16c401f985a318c7e2c77689245cd
Mateusz Walczyk <mattiusw@gmail.com>
2023-09-12 12:05:03 +0200

    Add particle effects

--- Snow_Boarder/CrashDetector.cs
+++ Snow_Boarder/CrashDetector.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashEffect; 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("CrashDetection", 1f); //Delay 1s
         }
     }
--- Snow_Boarder/FinishLine.cs
+++ Snow_Boarder/FinishLine.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishEffect;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", 1f); //Delay 1s
         }
     }
