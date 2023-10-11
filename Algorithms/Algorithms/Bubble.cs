fc8f47c7c77115d690633d7b82c668cc5be11800
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-11 14:21:56 +0200

    Bubble sort

---
+++ Algorithms/Algorithms/Bubble.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Bubble
+    {
+        public static int[] BubbleSort(int[] tab)
+        { 
+            for (int i = 1; i < tab.Length; i++ )
+            {
+                for (int j = 0; j < tab.Length; j++)
+                {
+                    if (tab[j] > tab[i]) //5
+                    {
+                        int temp = tab[j];
+                        tab[j] = tab[i];
+                        tab[i] = temp;
+                    }
+                }
+            }
+            return tab;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithms");
+            int[] table = { 1, 4, 3, 2, 6, 0, 9 };
+            Bubble.BubbleSort(table);
+            // Method to change object table on string
+            Console.WriteLine(string.Join(",", table));
+        }
+    }
+}
